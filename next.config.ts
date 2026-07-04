7a48950a5b7c4889f0bbd278b9ad522f26d7944c
smo5ke <abdokteb.1@gmail.com>
2026-07-05 01:37:31 +0300

    Add GitHub Actions deployment workflow for Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = '';
+let basePath = '';
+
+// If we are deploying to GitHub Pages, we need to set the base path correctly
+// because it runs at https://username.github.io/repo-name/
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '');
+  if (repo) {
+    assetPrefix = `/${repo}/`;
+    basePath = `/${repo}`;
+  }
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Required for GitHub Pages deployment
+  output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  
+  // Disable image optimization since GitHub Pages doesn't support the Next.js image server
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
